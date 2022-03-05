f15b07d32afc8f77f8aea6e051ff006030cd61e0
Clesyo <clesyo.silva@gmail.com>
2022-03-05 14:44:35 -0300

    Primento commit, implementado as parametrizacoes de configuracao do servidor

---
+++ src/test/java/br/com/delivery/config/DeliveryConfigServerApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.delivery.config;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DeliveryConfigServerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
